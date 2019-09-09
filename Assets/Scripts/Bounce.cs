02e1a81561d2dfdcd2c7c24c489664c361672501
killersshadow <killersshadow@gmail.com>
2019-09-09 21:08:25 +0200

    start game

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    [SerializeField] private float force = 1;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.up * force);
+    }
+
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 1;
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0) * speed * Time.deltaTime);
+
+    }
+}
