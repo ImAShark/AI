dc6fcb5079847fe341a0f6060696a400ffd84e59
killersshadow <32833605+killersshadow@users.noreply.github.com>
2019-09-16 11:41:13 +0200

    mutaties

--- Assets/Scripts/AI/Brain.cs
+++ Assets/Scripts/AI/Brain.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Brain : MonoBehaviour
 {
     private List<Vector3> directions = new List<Vector3>();
+    private List<Vector3> alfaPath = new List<Vector3>();
     [SerializeField] private float timer = 1;
-    [SerializeField] private float reset,y,x;
-    private int step,yeetPoints;
-    private bool isDead = false;
+    private float reset,y,x;
+    private int step,aStep,yeetPoints;
+    private bool isDead = false, firstTime = false, hasAlfaPath = false;
 
     void Awake()
     {
@@ -19,9 +20,36 @@ public class Brain : MonoBehaviour
         x = transform.position.x;
     }
 
+    void Start()
+    {
+        alfaPath = GameObject.Find("PopulationController").GetComponent<Population>().getAlfaPath();
+        if (alfaPath.Count == 0)
+        {
+            hasAlfaPath = false;
+        }
+    }
+
     void Update()
     {
-        if (!isDead)
+        Move();
+                
+    }
+
+    private void Move()
+    {
+        if (hasAlfaPath)
+        {
+            if (timer <= 0 && aStep <= alfaPath.Count)
+            {
+                MoveToVector();
+                timer = reset;
+            }
+            else
+            {
+                timer = timer - Time.deltaTime;
+            }
+        }
+        else if (!isDead && firstTime)
         {
             if (timer <= 0)
             {
@@ -33,7 +61,8 @@ public class Brain : MonoBehaviour
                 timer = timer - Time.deltaTime;
             }
         }
-                
+
+        
     }
 
     private void MoveRandomly(int times)
@@ -47,14 +76,32 @@ public class Brain : MonoBehaviour
             step++;
             if (isDead)
             {
+                step = 0;
                 break;
             }
+        }
+    }
+
+    private void MoveToVector()
+    {
+        if (hasAlfaPath)
+        {
+            gameObject.transform.position = alfaPath[aStep];
+            aStep++;
         }
     }
 
     private void Die()
     {
         isDead = true;
+        firstTime = false;
+    }
+
+    public void SetAlfaPath(bool b, List<Vector3> p)
+    {
+        hasAlfaPath = b;
+        alfaPath = p;
+        isDead = false;
     }
 
     private void OnTriggerEnter(Collider col)
@@ -74,5 +121,12 @@ public class Brain : MonoBehaviour
     public List<Vector3> getPath()
     {
         return directions;
+    }
+
+    public Vector3 getRandomPath()
+    {
+        float random = Random.Range(x - 9.5f, x + 9.5f);
+        Vector3 t = new Vector3(random, y, 0);
+        return t;
     }
 }
--- Assets/Scripts/AI/Population.cs
+++ Assets/Scripts/AI/Population.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Population : MonoBehaviour
 {
+    [SerializeField] private GameObject subject;
     private List<GameObject> parents = new List<GameObject>();
     private List<Vector3> aDirections = new List<Vector3>();
-    private int amount,high;
+    private int amount,high, deaths;
     private GameObject alfa, mutation;
 
     void Start()
@@ -18,8 +19,8 @@ public class Population : MonoBehaviour
            parents.Add(players[i]);
            var casulties = parents[i].gameObject.transform.parent.GetComponentInChildren<Ball>();
            casulties.BallDies += StartProcces;
-        }   
-
+        }
+        Debug.Log(amount);
     }
 
     void Update()
@@ -29,8 +30,8 @@ public class Population : MonoBehaviour
 
     private void StartProcces()
     {
-        
-        if (amount == GameObject.FindGameObjectsWithTag("Player").Length)
+        BallDead();
+        if (deaths == amount)
         {
             for (int i = 0; i < amount; i++)
             {
@@ -40,10 +41,52 @@ public class Population : MonoBehaviour
                 {
                     y = high;
                     alfa = t;
-                    Debug.Log("alfa " + alfa);
+                    Debug.Log("alfa " + alfa.transform.parent.gameObject.name);
+                    Mutate();
                 }
             }
         }
+    }
+
+    private void Mutate()
+    {
+        aDirections = alfa.GetComponent<Brain>().getPath();
+
+        for (int i = 0; i < aDirections.Count; i++)
+        {
+            int t = Random.Range(0,100);
+            if (t >= 5)
+            {
+                aDirections[i] = alfa.GetComponent<Brain>().getRandomPath();
+            }
+        }
+        RePopulate();
+    }
+
+    public List<Vector3> getAlfaPath()
+    {
+        return aDirections;
+    }
+
+    private void RePopulate()
+    {
+        bool proccesed = false;
+        if (!proccesed)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                Vector3 pos = parents[i].gameObject.transform.position;
+                Destroy(parents[i].gameObject);
+                Instantiate(subject, pos, Quaternion.identity);
+            }
+            proccesed = true;
+        }
+        
+    }
+
+    private void BallDead()
+    {
+        deaths++;
     }
 
 
